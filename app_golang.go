1c94c16fb3768a2b2266b40f77243a3a45e48c90
verter187 <verter187@inbox.ru>
2023-03-29 21:10:04 +0300

    Switch

--- app_golang.go
+++ app_golang.go
@@ -1,23 +1,51 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
-	if 7%2 == 0 {
-		fmt.Println("7 is even")
-	} else {
-		fmt.Println("7 is odd")
+	i := 2
+	fmt.Print("Write ", i, "as")
+	switch i {
+	case 1:
+		fmt.Println("one")
+	case 2:
+		fmt.Println("two")
+	case 3:
+		fmt.Println("three")
 	}
 
-	if 8%4 == 0 {
-		fmt.Println("8 is division by 4")
+	switch time.Now().Weekday() {
+	case time.Saturday, time.Sunday:
+		fmt.Println("It's the weekend")
+	default:
+		fmt.Println("It's a weekend")
 	}
 
-	if num := 9; num < 0 {
-		fmt.Println(num, "is negative")
-	} else if num < 10 {
-		fmt.Println(num, "has 1 digit")
-	} else {
-		fmt.Println(num, "has multiple digits")
+	t := time.Now()
+	switch {
+	case t.Hour() < 12:
+		fmt.Println("It's before noon")
+	default:
+		fmt.Println("It's after noon")
 	}
+
+	whatAmI := func(i interface{}) {
+
+		switch t := i.(type) {
+		case bool:
+			fmt.Println("I'm a boolean")
+		case int:
+			fmt.Println("I'm a number")
+		default:
+			fmt.Printf("Don't know type %T\n", t)
+		}
+
+	}
+
+	whatAmI(true)
+	whatAmI(1)
+	whatAmI("hey")
 }
