23226b218b2f37f72816a558ce2340f7f656d992
verter187 <verter187@inbox.ru>
2023-03-29 22:07:19 +0300

    Срезы (Slices)

--- app_golang.go
+++ app_golang.go
@@ -3,23 +3,44 @@ package main
 import "fmt"
 
 func main() {
-	var a [5]int
-	fmt.Println("emp:", a)
+	s := make([]string, 3)
+	fmt.Println("emp", s)
 
-	a[4] = 100
-	fmt.Println("set", a)
-	fmt.Println("get", a[4])
+	s[0] = "a"
+	s[1] = "b"
+	s[2] = "c"
+	fmt.Println("set:", s)
+	fmt.Println("get:", s[2])
 
-	fmt.Println("len", len(a))
+	fmt.Println("len:", len(s))
 
-	b := [5]int{1, 2, 3, 4, 5}
-	fmt.Println("dcl:", b)
+	s = append(s, "d")
+	s = append(s, "e", "f")
+	fmt.Println("apd", s)
 
-	var twoD [2][3]int
-	for i := 0; i < 2; i++ {
-		for j := 0; j < 3; j++ {
+	c := make([]string, len(s))
+	copy(c, s)
+	fmt.Println("cpy", c)
+
+	l := s[2:5]
+	fmt.Println("sl1", l)
+
+	l = s[:5]
+	fmt.Println("sl2:", l)
+
+	l = s[2:]
+	fmt.Println("sl3:", l)
+
+	t := []string{"g", "h", "i"}
+	fmt.Println("dcl:", t)
+
+	twoD := make([][]int, 3)
+	for i := 0; i < 3; i++ {
+		innerLen := i + 1
+		twoD[i] = make([]int, innerLen)
+		for j := 0; j < innerLen; j++ {
 			twoD[i][j] = i + j
 		}
-		fmt.Println("2d: ", twoD)
 	}
+	fmt.Println("2d: ", twoD)
 }
