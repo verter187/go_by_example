547e9d8d5aa4b9f51e8ae0252d7f743bcc488ea4
verter187 <verter187@inbox.ru>
2023-03-29 18:39:17 +0300

    Типы данных (Values)

--- app_golang.go
+++ app_golang.go
@@ -3,10 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hello world")
-	var b int = 0
-	for i := 0; i < 10000000000; i++ {
-		b = b + 2
-	}
-	fmt.Println("всего:", b)
+	fmt.Println("go" + "lang")
+
+	fmt.Println("1+1 =", 1+1)
+	fmt.Println("7.0/3.0 =", 7.0/3.0)
+
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
 }
