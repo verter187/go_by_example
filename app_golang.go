a7e2515921ae5a03cff6103a4b93603d5c0245ac
verter187 <verter187@inbox.ru>
2023-03-29 19:25:51 +0300

    If/Else

--- app_golang.go
+++ app_golang.go
@@ -3,26 +3,21 @@ package main
 import "fmt"
 
 func main() {
-	i := 1
-	for i < 3 {
-		fmt.Println(i)
-		i += 1
+	if 7%2 == 0 {
+		fmt.Println("7 is even")
+	} else {
+		fmt.Println("7 is odd")
 	}
 
-	for j := 7; j <= 9; j++ {
-		fmt.Println(j)
+	if 8%4 == 0 {
+		fmt.Println("8 is division by 4")
 	}
 
-	for {
-		fmt.Println("loop")
-		break
+	if num := 9; num < 0 {
+		fmt.Println(num, "is negative")
+	} else if num < 10 {
+		fmt.Println(num, "has 1 digit")
+	} else {
+		fmt.Println(num, "has multiple digits")
 	}
-
-	for n := 0; n <= 5; n++ {
-		if n%2 == 0 {
-			continue
-		}
-		fmt.Println(n)
-	}
-
 }
