2ecf72cd66818ff1187b420ff7bf279e8b9ae8a9
verter187 <verter187@inbox.ru>
2023-04-01 22:14:37 +0300

    Рекурсия (Recursion)

--- app_golang.go
+++ app_golang.go
@@ -2,22 +2,13 @@ package main
 
 import "fmt"
 
-func intSeq() func() int {
-	i := 0
-	return func() int {
-		i++
-		return i
+func fact(n int) int {
+	if n == 0 {
+		return 1
 	}
+	return n * fact(n-1)
 }
 
 func main() {
-	nextInt := intSeq()
-
-	fmt.Println(nextInt())
-	fmt.Println(nextInt())
-	fmt.Println(nextInt())
-
-	newInts := intSeq()
-	fmt.Println(newInts())
-	fmt.Println(newInts())
+	fmt.Println(fact(7))
 }
