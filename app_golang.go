a4e8dd1bdea67f387ad9383c2d15da391c67b3ef
verter187 <verter187@inbox.ru>
2023-04-01 22:22:16 +0300

    Замыкания (Closures)

--- app_golang.go
+++ app_golang.go
@@ -2,13 +2,21 @@ package main
 
 import "fmt"
 
-func fact(n int) int {
-	if n == 0 {
-		return 1
+func intSeq() func() int {
+	i := 0
+	return func() int {
+		i++
+		return i
 	}
-	return n * fact(n-1)
 }
 
 func main() {
-	fmt.Println(fact(7))
+	nextInt := intSeq()
+
+	fmt.Println(nextInt())
+	fmt.Println(nextInt())
+	fmt.Println(nextInt())
+
+	newInts := intSeq()
+	fmt.Print(newInts())
 }
