7e747b2ecfd5a54800d25621f0a45799cd4f5b2f
verter187 <verter187@inbox.ru>
2023-04-01 18:00:59 +0300

    Функции с переменным числом аргументов (Variadic Functions)

--- app_golang.go
+++ app_golang.go
@@ -1,25 +1,21 @@
 package main
 
-import (
-	"fmt"
-	"strconv"
-)
+import "fmt"
 
-func plus(a int, b int) int {
-	return a + b
-}
+func sum(nums ...int) {
+	fmt.Print(nums, " ")
+	total := 0
+	for _, num := range nums {
+		total += num
+	}
+	fmt.Println(total)
 
-func plusPlus(a, b, c int) (string, int, string, bool) {
-	str := strconv.Itoa(a) + "+" + strconv.Itoa(b) + "+" + strconv.Itoa(c) + "="
-
-	return str, a + b + c, "test", true
 }
 
 func main() {
-	res := plus(1, 2)
-	fmt.Println(res)
+	sum(1, 2)
+	sum(1, 2, 3)
 
-	str, res, test, _ := plusPlus(1, 2, 3)
-	fmt.Println(str, res, test)
-
+	nums := []int{1, 2, 3, 4}
+	sum(nums...)
 }
