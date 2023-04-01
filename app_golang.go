8a2333a87b2841d88008d1fe95813c96a047d532
verter187 <verter187@inbox.ru>
2023-04-01 22:10:13 +0300

    Замыкания (Closures)

--- app_golang.go
+++ app_golang.go
@@ -2,20 +2,22 @@ package main
 
 import "fmt"
 
-func sum(nums ...int) {
-	fmt.Print(nums, " ")
-	total := 0
-	for _, num := range nums {
-		total += num
+func intSeq() func() int {
+	i := 0
+	return func() int {
+		i++
+		return i
 	}
-	fmt.Println(total)
-
 }
 
 func main() {
-	sum(1, 2)
-	sum(1, 2, 3)
+	nextInt := intSeq()
 
-	nums := []int{1, 2, 3, 4}
-	sum(nums...)
+	fmt.Println(nextInt())
+	fmt.Println(nextInt())
+	fmt.Println(nextInt())
+
+	newInts := intSeq()
+	fmt.Println(newInts())
+	fmt.Println(newInts())
 }
