bce7365da7280df6357a1eb00a09093e7b80f5dc
verter187 <verter187@inbox.ru>
2023-04-01 17:44:15 +0300

    Функции с множественным возвратом (Multiple Return Values)

--- app_golang.go
+++ app_golang.go
@@ -9,17 +9,17 @@ func plus(a int, b int) int {
 	return a + b
 }
 
-func plusPlus(a, b, c int, res string) int {
-	res = strconv.Itoa(a) + "+" + strconv.Itoa(b) + "+" + strconv.Itoa(c) + "="
-	fmt.Println(res)
-	return a + b + c
+func plusPlus(a, b, c int) (string, int, string, bool) {
+	str := strconv.Itoa(a) + "+" + strconv.Itoa(b) + "+" + strconv.Itoa(c) + "="
+
+	return str, a + b + c, "test", true
 }
 
 func main() {
 	res := plus(1, 2)
 	fmt.Println(res)
 
-	var str string
-	res = plusPlus(1, 2, 3, str)
-	fmt.Println(str, res)
+	str, res, test, _ := plusPlus(1, 2, 3)
+	fmt.Println(str, res, test)
+
 }
