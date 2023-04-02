e6dd00fbcd962ce3ad871a25053c7b82ea890085
verter187 <verter187@inbox.ru>
2023-04-02 19:01:16 +0300

    Ошибки (Errors)

--- app_golang.go
+++ app_golang.go
@@ -1,48 +1,52 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"math"
 )
 
-type geometry interface {
-	area() float64
-	perim() float64
+func f1(arg int) (int, error) {
+	if arg == 42 {
+		return -1, errors.New("can't work with 42")
+	}
+	return arg + 3, nil
 }
 
-type rect struct {
-	width, height float64
+type argError struct {
+	arg  int
+	prob string
 }
 
-type circle struct {
-	radius float64
+func (e *argError) Error() string {
+	return fmt.Sprintf("%d - %s", e.arg, e.prob)
 }
 
-func (r rect) area() float64 {
-	return r.width * r.height
-}
-
-func (r rect) perim() float64 {
-	return 2*r.width + 2*r.height
-}
-
-func (c circle) area() float64 {
-	return math.Pi * c.radius * c.radius
-}
-
-func (c circle) perim() float64 {
-	return 2 * math.Pi * c.radius
-}
-
-func measure(g geometry) {
-	fmt.Println(g)
-	fmt.Println(g.area())
-	fmt.Println(g.perim())
+func f2(arg int) (int, error) {
+	if arg == 42 {
+		return -1, &argError{arg, "can't work with it'"}
+	}
+	return arg + 3, nil
 }
 
 func main() {
-	r := rect{width: 3, height: 4}
-	c := circle{radius: 5}
-	measure(r)
-	measure(c)
+	for _, i := range []int{7, 42} {
+		if r, e := f1(i); e != nil {
+			fmt.Println("f1 failed:", e)
+		} else {
+			fmt.Println("f1 worked:", r)
+		}
+	}
+	for _, i := range []int{7, 42} {
+		if r, e := f2(i); e != nil {
+			fmt.Println("f2 failed:", e)
+		} else {
+			fmt.Println("f2 worked:", r)
+		}
+	}
+
+	_, e := f2(42)
+	if ae, ok := e.(*argError); ok {
+		fmt.Println(ae.arg)
+		fmt.Println(ae.prob)
+	}
 }
