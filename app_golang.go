f75703b59bbe913e5898a94ba6a27249b09a1b96
verter187 <verter187@inbox.ru>
2023-04-02 15:56:59 +0300

    Структуры (Structs)

--- app_golang.go
+++ app_golang.go
@@ -2,23 +2,34 @@ package main
 
 import "fmt"
 
-func zeroval(ival int) {
-	ival = 0
+type person struct {
+	name string
+	age  int
 }
 
-func zeroptr(iptr *int) {
-	*iptr = 7
+func NewPerson(name string) *person {
+	p := person{name: name}
+	p.age = 42
+	return &p
 }
 
 func main() {
-	i := 1
-	fmt.Println("initial:", i)
+	fmt.Println(person{"Bob", 20})
+	fmt.Println(person{name: "Alice", age: 30})
 
-	zeroval(i)
-	fmt.Println("zeroval:", i)
+	fmt.Println(person{name: "Fred"})
+	fmt.Println(&person{name: "Ann", age: 40})
 
-	zeroptr(&i)
-	fmt.Println("zeroptr:", i)
+	fmt.Println(NewPerson("Jhon"))
 
-	fmt.Println("pointer:", &i)
+	s := person{name: "Sean", age: 50}
+	fmt.Println(s.name)
+
+	sp := &s
+	fmt.Println(sp.age)
+
+	sp.age = 51
+	fmt.Println(sp.age)
+	fmt.Println(s.age)
+
 }
