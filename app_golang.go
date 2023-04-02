cd54afa455301fe308f08ab6664aeef59fcba88f
verter187 <verter187@inbox.ru>
2023-04-02 16:08:29 +0300

    Методы (Methods)

--- app_golang.go
+++ app_golang.go
@@ -2,34 +2,26 @@ package main
 
 import "fmt"
 
-type person struct {
-	name string
-	age  int
+type rect struct {
+	width, height int
 }
 
-func NewPerson(name string) *person {
-	p := person{name: name}
-	p.age = 42
-	return &p
+func (r *rect) area() int {
+	return r.width * r.height
+}
+
+func (r rect) perim() int {
+	return 2*r.width + 2*r.height
 }
 
 func main() {
-	fmt.Println(person{"Bob", 20})
-	fmt.Println(person{name: "Alice", age: 30})
+	r := rect{width: 10, height: 5}
+	fmt.Println("area: ", r.area())
+	fmt.Println("perim: ", r.perim())
 
-	fmt.Println(person{name: "Fred"})
-	fmt.Println(&person{name: "Ann", age: 40})
+	rp := &r
 
-	fmt.Println(NewPerson("Jhon"))
-
-	s := person{name: "Sean", age: 50}
-	fmt.Println(s.name)
-
-	sp := &s
-	fmt.Println(sp.age)
-
-	sp.age = 51
-	fmt.Println(sp.age)
-	fmt.Println(s.age)
-
+	rp.width = 15
+	fmt.Println("area: ", rp.area())
+	fmt.Println("perim: ", rp.perim())
 }
