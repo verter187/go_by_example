78c2dd5ea035d1c86e915d386eb5546f484d26f0
verter187 <verter187@inbox.ru>
2023-04-05 19:24:58 +0300

    Направления канала (Channel Directions)

--- app_golang.go
+++ app_golang.go
@@ -1,22 +1,20 @@
 package main
 
-import (
-	"fmt"
-	"time"
-)
+import "fmt"
 
-func worker(done chan bool) {
-	fmt.Print("working...")
-	time.Sleep(time.Second)
-	fmt.Println("done")
+func ping(pings chan<- string, msg string) {
+	pings <- msg
+}
 
-	done <- true
-
+func pong(pings <-chan string, pongs chan<- string) {
+	msg := <-pings
+	pongs <- msg
 }
 
 func main() {
-	done := make(chan bool, 1)
-	go worker(done)
-
-	<-done
+	pings := make(chan string, 1)
+	pongs := make(chan string, 1)
+	ping(pings, "passed message")
+	pong(pings, pongs)
+	fmt.Println(<-pongs)
 }
