854f3ecbeacf5b49364d4eac7af31c8aeace3457
verter187 <verter187@inbox.ru>
2023-04-05 19:40:27 +0300

    Select

--- app_golang.go
+++ app_golang.go
@@ -1,20 +1,30 @@
 package main
 
-import "fmt"
-
-func ping(pings chan<- string, msg string) {
-	pings <- msg
-}
-
-func pong(pings <-chan string, pongs chan<- string) {
-	msg := <-pings
-	pongs <- msg
-}
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
-	pings := make(chan string, 1)
-	pongs := make(chan string, 1)
-	ping(pings, "passed message")
-	pong(pings, pongs)
-	fmt.Println(<-pongs)
+	c1 := make(chan string)
+	c2 := make(chan string)
+
+	go func() {
+		time.Sleep(1 * time.Second)
+		c1 <- "one"
+	}()
+
+	go func() {
+		time.Sleep(2 * time.Second)
+		c2 <- "two"
+	}()
+
+	for i := 0; i < 2; i++ {
+		select {
+		case msg1 := <-c1:
+			fmt.Println("received", msg1)
+		case msg2 := <-c2:
+			fmt.Println("received", msg2)
+		}
+	}
 }
