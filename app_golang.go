8cf544022bf061ff29c9973d292d494da0cd4a1e
verter187 <verter187@inbox.ru>
2023-04-05 21:21:34 +0300

    Закрытие каналов (Closing Channels)

--- app_golang.go
+++ app_golang.go
@@ -3,30 +3,28 @@ package main
 import "fmt"
 
 func main() {
-	messages := make(chan string)
-	signals := make(chan bool)
+	jobs := make(chan int, 5)
+	done := make(chan bool)
 
-	select {
-	case msg := <-messages:
-		fmt.Println("received message: ", msg)
-	default:
-		fmt.Println("no message received")
-	}
+	go func() {
+		for {
+			j, more := <-jobs
+			if more {
+				fmt.Println("received job", j)
+			} else {
+				fmt.Println("received all jobs", j)
+				done <- true
+				return
+			}
+		}
+	}()
 
-	msg := "h1"
-	select {
-	case messages <- msg:
-		fmt.Println("sent message: ", msg)
-	default:
-		fmt.Println("no message sent")
+	for j := 1; j <= 3; j++ {
+		jobs <- j
+		fmt.Println("sent job", j)
 	}
+	close(jobs)
+	fmt.Println("sent all jobs")
 
-	select {
-	case msg := <-messages:
-		fmt.Println("received message: ", msg)
-	case sig := <-signals:
-		fmt.Println("received signal: ", sig)
-	default:
-		fmt.Println("no activity")
-	}
+	<-done
 }
