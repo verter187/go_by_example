5c77cd2e681b3fe1fa79040d48c7e8d675ced16c
verter187 <verter187@inbox.ru>
2023-04-05 20:10:59 +0300

    Тайм-ауты (Timeouts)

--- app_golang.go
+++ app_golang.go
@@ -6,25 +6,30 @@ import (
 )
 
 func main() {
-	c1 := make(chan string)
-	c2 := make(chan string)
 
-	go func() {
-		time.Sleep(1 * time.Second)
-		c1 <- "one"
-	}()
-
+	c1 := make(chan string, 1)
 	go func() {
 		time.Sleep(2 * time.Second)
-		c2 <- "two"
+		c1 <- "result 1"
 	}()
 
-	for i := 0; i < 2; i++ {
-		select {
-		case msg1 := <-c1:
-			fmt.Println("received", msg1)
-		case msg2 := <-c2:
-			fmt.Println("received", msg2)
-		}
+	select {
+	case res := <-c1:
+		fmt.Println(res)
+	case <-time.After(3 * time.Second):
+		fmt.Println("timeout 1")
+	}
+
+	c2 := make(chan string, 1)
+	go func() {
+		time.Sleep(2 * time.Second)
+		c2 <- "result 2"
+	}()
+
+	select {
+	case res := <-c2:
+		fmt.Println(res)
+	case <-time.After(3 * time.Second):
+		fmt.Println("timeout 2")
 	}
 }
