b2db081880ac6a4051cd772d491f5bee0066701f
verter187 <verter187@inbox.ru>
2023-04-05 21:00:20 +0300

    Неблокируемые операции в каналах (Non-Blocking Channel Operations)

--- app_golang.go
+++ app_golang.go
@@ -1,35 +1,32 @@
 package main
 
-import (
-	"fmt"
-	"time"
-)
+import "fmt"
 
 func main() {
-
-	c1 := make(chan string, 1)
-	go func() {
-		time.Sleep(2 * time.Second)
-		c1 <- "result 1"
-	}()
+	messages := make(chan string)
+	signals := make(chan bool)
 
 	select {
-	case res := <-c1:
-		fmt.Println(res)
-	case <-time.After(3 * time.Second):
-		fmt.Println("timeout 1")
+	case msg := <-messages:
+		fmt.Println("received message: ", msg)
+	default:
+		fmt.Println("no message received")
 	}
 
-	c2 := make(chan string, 1)
-	go func() {
-		time.Sleep(2 * time.Second)
-		c2 <- "result 2"
-	}()
+	msg := "h1"
+	select {
+	case messages <- msg:
+		fmt.Println("sent message: ", msg)
+	default:
+		fmt.Println("no message sent")
+	}
 
 	select {
-	case res := <-c2:
-		fmt.Println(res)
-	case <-time.After(3 * time.Second):
-		fmt.Println("timeout 2")
+	case msg := <-messages:
+		fmt.Println("received message: ", msg)
+	case sig := <-signals:
+		fmt.Println("received signal: ", sig)
+	default:
+		fmt.Println("no activity")
 	}
 }
