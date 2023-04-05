848c8b5afcd3d425be5414cc24d12c59a03cc13f
verter187 <verter187@inbox.ru>
2023-04-05 21:34:18 +0300

    Перебор значений из каналов (Range over Channels)

--- app_golang.go
+++ app_golang.go
@@ -3,28 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	jobs := make(chan int, 5)
-	done := make(chan bool)
+	queue := make(chan string, 2)
+	queue <- "one"
+	queue <- "two"
+	close(queue)
 
-	go func() {
-		for {
-			j, more := <-jobs
-			if more {
-				fmt.Println("received job", j)
-			} else {
-				fmt.Println("received all jobs", j)
-				done <- true
-				return
-			}
-		}
-	}()
-
-	for j := 1; j <= 3; j++ {
-		jobs <- j
-		fmt.Println("sent job", j)
+	for elem := range queue {
+		fmt.Println(elem)
 	}
-	close(jobs)
-	fmt.Println("sent all jobs")
-
-	<-done
 }
