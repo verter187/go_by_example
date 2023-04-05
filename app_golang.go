cc71562301f022e9c1d2d15be8960bc3cf2882e6
verter187 <verter187@inbox.ru>
2023-04-05 21:57:07 +0300

    Таймеры (Timers)

--- app_golang.go
+++ app_golang.go
@@ -1,14 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
-	queue := make(chan string, 2)
-	queue <- "one"
-	queue <- "two"
-	close(queue)
+	timer1 := time.NewTimer(2 * time.Second)
 
-	for elem := range queue {
-		fmt.Println(elem)
+	<-timer1.C
+	fmt.Println("Timer 1 expired")
+
+	timer2 := time.NewTimer(1 * time.Second)
+	go func() {
+		<-timer2.C
+		fmt.Println("Timer 2 expired")
+	}()
+	stop2 := timer2.Stop()
+	if stop2 {
+		fmt.Println("Time 2 stopped")
 	}
+	timer3 := time.NewTimer(3 * time.Second)
+	<-timer3.C
+	fmt.Println("Timer 3 expired")
+
 }
