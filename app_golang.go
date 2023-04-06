8d3853ce6b2799872fc187efb9d292f1198dcecb
verter187 <verter187@inbox.ru>
2023-04-06 21:54:32 +0300

    Атомарные счетчики (Atomic Counters)

--- app_golang.go
+++ app_golang.go
@@ -2,43 +2,27 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"sync"
+	"sync/atomic"
 )
 
 func main() {
-	request := make(chan int, 5)
-	for i := 0; i < 5; i++ {
-		request <- i
+
+	var ops uint64
+
+	var wg sync.WaitGroup
+
+	for i := 0; i < 50; i++ {
+		wg.Add(1)
+
+		go func() {
+			for c := 0; c < 1000; c++ {
+				atomic.AddUint64(&ops, 1)
+			}
+			wg.Done()
+		}()
 	}
-	close(request)
+	wg.Wait()
 
-	limiter := time.Tick(200 * time.Millisecond)
-
-	for req := range request {
-		<-limiter
-		fmt.Println("request", req, time.Now())
-	}
-
-	burstyLimiter := make(chan time.Time, 3)
-
-	for i := 0; i < 3; i++ {
-		burstyLimiter <- time.Now()
-	}
-
-	go func() {
-		for t := range time.Tick(200 * time.Millisecond) {
-			burstyLimiter <- t
-		}
-	}()
-
-	burstyRequests := make(chan int, 5)
-	for i := 0; i < 5; i++ {
-		burstyRequests <- i
-	}
-	close(burstyRequests)
-
-	for req := range burstyRequests {
-		<-burstyLimiter
-		fmt.Println("request", req, time.Now())
-	}
+	fmt.Println("ops", ops)
 }
