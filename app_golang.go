4bd91f03289aa1e442072cd51a4a48b00183e5e9
verter187 <verter187@inbox.ru>
2023-04-06 21:04:56 +0300

    WaitGroups

--- app_golang.go
+++ app_golang.go
@@ -2,35 +2,25 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
-func worker(id int, jobs <-chan int, results chan<- int) {
+func worker(id int, wg *sync.WaitGroup) {
+	fmt.Printf("Worker %d starting\n", id)
 
-	for j := range jobs {
-		fmt.Println("worker", id, "started job", j)
-		time.Sleep(time.Second)
-		fmt.Println("worker", id, "finished job", j)
-		results <- j * 2
+	time.Sleep(time.Second)
+	fmt.Printf("Worker %d done\n", id)
 
-	}
+	wg.Done()
 }
 
 func main() {
-	jobs := make(chan int, 100)
-	results := make(chan int, 100)
+	var wg sync.WaitGroup
 
-	for w := 1; w <= 3; w++ {
-		go worker(w, jobs, results)
+	for i := 1; i <= 5; i++ {
+		wg.Add(1)
+		go worker(i, &wg)
 	}
-
-	for j := 1; j <= 5; j++ {
-		jobs <- j
-	}
-	close(jobs)
-
-	for a := 1; a <= 5; a++ {
-		<-results
-	}
-
+	wg.Wait()
 }
