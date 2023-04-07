e99d212c0a96303150a0bf06a64f20e6f1e5bf0e
verter187 <verter187@inbox.ru>
2023-04-07 19:26:26 +0300

    Мьютексы (Mutexes)

--- app_golang.go
+++ app_golang.go
@@ -2,27 +2,58 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"sync"
 	"sync/atomic"
+	"time"
 )
 
 func main() {
 
-	var ops uint64
+	var state = make(map[int]int)
 
-	var wg sync.WaitGroup
+	var mutex = &sync.Mutex{}
 
-	for i := 0; i < 50; i++ {
-		wg.Add(1)
+	var readOps uint64
+	var writeOps uint64
 
+	for r := 0; r < 100; r++ {
 		go func() {
-			for c := 0; c < 1000; c++ {
-				atomic.AddUint64(&ops, 1)
+			total := 0
+			for {
+				key := rand.Intn(5)
+				mutex.Lock()
+				total += state[key]
+				mutex.Unlock()
+				atomic.AddUint64(&readOps, 1)
+
+				time.Sleep(time.Millisecond)
 			}
-			wg.Done()
 		}()
 	}
-	wg.Wait()
 
-	fmt.Println("ops", ops)
+	for w := 0; w < 10; w++ {
+		go func() {
+			for {
+				key := rand.Intn(5)
+				val := rand.Intn(100)
+				mutex.Lock()
+				state[key] = val
+				mutex.Unlock()
+				atomic.AddUint64(&writeOps, 1)
+				time.Sleep(time.Millisecond)
+			}
+		}()
+	}
+
+	time.Sleep(time.Second)
+
+	readOpsFinal := atomic.LoadUint64(&readOps)
+	fmt.Println("readOps:", readOpsFinal)
+	writeOpsFinal := atomic.LoadUint64(&writeOps)
+	fmt.Println("writeOps:", writeOpsFinal)
+
+	mutex.Lock()
+	fmt.Println("state:", state)
+	mutex.Unlock()
 }
