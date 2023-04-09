0e06f85d62c2d16ae403c7df3c9359c9c6713b45
verter187 <verter187@inbox.ru>
2023-04-09 15:37:50 +0300

    Управление состоянием горутин (Stateful Goroutines)

--- app_golang.go
+++ app_golang.go
@@ -3,57 +3,74 @@ package main
 import (
 	"fmt"
 	"math/rand"
-	"sync"
 	"sync/atomic"
 	"time"
 )
 
+type readOp struct {
+	key  int
+	resp chan int
+}
+
+type writeOp struct {
+	key  int
+	val  int
+	resp chan bool
+}
+
 func main() {
-
-	var state = make(map[int]int)
-
-	var mutex = &sync.Mutex{}
 
 	var readOps uint64
 	var writeOps uint64
 
+	reads := make(chan readOp)
+	writes := make(chan writeOp)
+
+	go func() {
+		var state = make(map[int]int)
+		for {
+			select {
+			case read := <-reads:
+				read.resp <- state[read.key]
+			case write := <-writes:
+				state[write.key] = write.val
+				write.resp <- true
+			}
+		}
+	}()
+
 	for r := 0; r < 100; r++ {
 		go func() {
-			total := 0
 			for {
-				key := rand.Intn(5)
-				mutex.Lock()
-				total += state[key]
-				mutex.Unlock()
+				read := readOp{
+					key:  rand.Intn(5),
+					resp: make(chan int),
+				}
+				reads <- read
+				<-read.resp
 				atomic.AddUint64(&readOps, 1)
-
 				time.Sleep(time.Millisecond)
 			}
 		}()
 	}
-
 	for w := 0; w < 10; w++ {
 		go func() {
 			for {
-				key := rand.Intn(5)
-				val := rand.Intn(100)
-				mutex.Lock()
-				state[key] = val
-				mutex.Unlock()
+				write := writeOp{
+					key:  rand.Intn(5),
+					val:  rand.Intn(100),
+					resp: make(chan bool)}
+				writes <- write
+				<-write.resp
 				atomic.AddUint64(&writeOps, 1)
-				time.Sleep(time.Millisecond)
+				time.Sleep((time.Millisecond))
 			}
 		}()
 	}
-
 	time.Sleep(time.Second)
 
 	readOpsFinal := atomic.LoadUint64(&readOps)
 	fmt.Println("readOps:", readOpsFinal)
 	writeOpsFinal := atomic.LoadUint64(&writeOps)
 	fmt.Println("writeOps:", writeOpsFinal)
-
-	mutex.Lock()
-	fmt.Println("state:", state)
-	mutex.Unlock()
 }
