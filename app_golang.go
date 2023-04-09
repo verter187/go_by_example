b017b2d18c2cf45de94451c74f725f92352d563c
verter187 <verter187@inbox.ru>
2023-04-09 16:01:49 +0300

    Panic

--- app_golang.go
+++ app_golang.go
@@ -1,20 +1,13 @@
 package main
 
-import (
-	"fmt"
-	"sort"
-)
-
-type byLength []string
-
-func (s byLength) Len() int { return len(s) }
-
-func (s byLength) Swap(i, j int) { s[i], s[j] = s[j], s[i] }
-
-func (s byLength) Less(i, j int) bool { return len(s[i]) < len(s[j]) }
+import "os"
 
 func main() {
-	fruits := []string{"peach", "banana", "kiwi"}
-	sort.Sort(byLength(fruits))
-	fmt.Println(fruits)
+
+	panic("a problem")
+
+	_, err := os.Create("/tmp/file")
+	if err != nil {
+		panic(err)
+	}
 }
