3c098637505b8e67e4bd79608162f13e880e50ab
verter187 <verter187@inbox.ru>
2023-04-11 20:54:14 +0300

    Time Formatting / Parsing

--- app_golang.go
+++ app_golang.go
@@ -6,19 +6,26 @@ import (
 )
 
 func main() {
+	p := fmt.Println
 
-	now := time.Now()
-	secs := now.Unix()
-	nanos := now.UnixNano()
+	t := time.Now()
+	p(t.Format(time.RFC3339))
 
-	fmt.Println(now)
+	t1, e := time.Parse(time.RFC3339, "2012-11-01T22:08:41+00:00")
+	p(t1)
+	p(e)
+	p(t.Format("3:04PM"))
+	p(t.Format("Mon Jan _2 15:04:05 2006"))
+	p(t.Format("2006-01-02T15:04:05.999999-07:00"))
+	form := "3 04 PM"
+	t2, e := time.Parse(form, "8 41 PM")
+	p(t2)
 
-	millis := nanos / 1000000
-	fmt.Println(secs)
-	fmt.Println(millis)
-	fmt.Println(nanos)
+	fmt.Printf("%d-%02d-%02dT%02d:%02d:%02d-00:00\n",
+		t.Year(), t.Month(), t.Day(),
+		t.Hour(), t.Minute(), t.Second())
 
-	fmt.Println(time.Unix(secs, 0))
-	fmt.Println(time.Unix(0, nanos))
-
+	ansic := "Mon Jan _2 15:04:05 2006"
+	_, e = time.Parse(ansic, "8:41PM")
+	p(e)
 }
