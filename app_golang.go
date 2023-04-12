24e3ea08ddcc4907e91e74d311852a19f5402716
verter187 <verter187@inbox.ru>
2023-04-12 20:27:29 +0300

    Запись файлов (Writing Files)

--- app_golang.go
+++ app_golang.go
@@ -3,7 +3,6 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"io"
 	"os"
 )
 
@@ -15,40 +14,31 @@ func check(e error) {
 
 func main() {
 
-	dat, err := os.ReadFile("/tmp/dat")
-	check(err)
-	fmt.Print(string(dat))
-
-	f, err := os.Open("/tmp/dat")
+	d1 := []byte("hello\ngo\n")
+	err := os.WriteFile("/tmp/dat1", d1, 0644)
 	check(err)
 
-	b1 := make([]byte, 18)
-	n1, err := f.Read(b1)
-	check(err)
-	fmt.Printf("%d bytes: %s\n", n1, string(b1[:n1]))
-
-	o2, err := f.Seek(6, 0)
-	check(err)
-	b2 := make([]byte, 4)
-	n2, err := f.Read(b2)
-	check(err)
-	fmt.Printf("%d bytes @ %d: ", n2, o2)
-	fmt.Printf("%v\n", string(b2[:n2]))
-
-	o3, err := f.Seek(6, 0)
-	check(err)
-	b3 := make([]byte, 2)
-	n3, err := io.ReadAtLeast(f, b3, 2)
-	check(err)
-	fmt.Printf("%d bytes @ %d: %s\n", n3, o3, string(b3))
-
-	_, err = f.Seek(0, 0)
+	f, err := os.Create("/tmp/dat2")
 	check(err)
 
-	r4 := bufio.NewReader(f)
-	b4, err := r4.Peek(5)
-	check(err)
-	fmt.Printf("5 bytes: %s\n", string(b4))
+	defer f.Close()
 
-	f.Close()
+	d2 := []byte{115, 111, 109, 101, 10}
+	n2, err := f.Write(d2)
+	check(err)
+	fmt.Printf("wrote %d bytes\n", n2)
+
+	n3, err := f.WriteString("writes\n")
+	check(err)
+	fmt.Printf("wrote %d bytes\n", n3)
+
+	f.Sync()
+
+	w := bufio.NewWriter(f)
+	n4, err := w.WriteString("buffered\n")
+	check(err)
+	fmt.Printf("wrote %d bytes\n", n4)
+
+	w.Flush()
+
 }
