0f0097c53d4bc12eb29ab8f28c811c1a24e04e18
verter187 <verter187@inbox.ru>
2023-04-12 21:23:35 +0300

    HTTP серверы (HTTP Servers)

--- app_golang.go
+++ app_golang.go
@@ -1,27 +1,28 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"net/http"
 )
 
+func hello(w http.ResponseWriter, req *http.Request) {
+
+	fmt.Fprintf(w, "Hello, bro!\n")
+}
+
+func headers(w http.ResponseWriter, req *http.Request) {
+
+	for name, headers := range req.Header {
+		for _, h := range headers {
+			fmt.Fprintf(w, "%v: %v\n", name, h)
+		}
+	}
+}
+
 func main() {
 
-	resp, err := http.Get("http://gobyexample.com")
-	if err != nil {
-		panic(err)
-	}
-	defer resp.Body.Close()
+	http.HandleFunc("/hello", hello)
+	http.HandleFunc("/headers", headers)
 
-	fmt.Println("Response status:", resp.Status)
-
-	scanner := bufio.NewScanner(resp.Body)
-	for i := 0; scanner.Scan() && i < 5; i++ {
-		fmt.Println(scanner.Text())
-	}
-
-	if err := scanner.Err(); err != nil {
-		panic(err)
-	}
+	http.ListenAndServe(":8090", nil)
 }
