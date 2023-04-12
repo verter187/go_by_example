6f87c3b576b13d8deed276a23b4b4f4f41a6621b
verter187 <verter187@inbox.ru>
2023-04-12 21:30:19 +0300

    Исполняющие процессы (Exec'ing Processes)

--- app_golang.go
+++ app_golang.go
@@ -1,40 +1,24 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
+	"os"
 	"os/exec"
+	"syscall"
 )
 
 func main() {
 
-	dateCmd := exec.Command("date")
-
-	dateOut, err := dateCmd.Output()
-	if err != nil {
-		panic(err)
+	binary, lookErr := exec.LookPath("ls")
+	if lookErr != nil {
+		panic(lookErr)
 	}
-	fmt.Println("> date")
-	fmt.Println(string(dateOut))
 
-	grepCmd := exec.Command("grep", "hello")
+	args := []string{"ls", "-a", "-l", "-h"}
 
-	grepIn, _ := grepCmd.StdinPipe()
-	grepOut, _ := grepCmd.StdoutPipe()
-	grepCmd.Start()
-	grepIn.Write([]byte("hello grep\ngoodbye grep"))
-	grepIn.Close()
-	grepBytes, _ := ioutil.ReadAll(grepOut)
-	grepCmd.Wait()
+	env := os.Environ()
 
-	fmt.Println("> grep hello")
-	fmt.Println(string(grepBytes))
-
-	lsCmd := exec.Command("bash", "-c", "ls -a -l -h")
-	lsOut, err := lsCmd.Output()
-	if err != nil {
-		panic(err)
+	execErr := syscall.Exec(binary, args, env)
+	if execErr != nil {
+		panic(execErr)
 	}
-	fmt.Println("> ls -a -l -h")
-	fmt.Println(string(lsOut))
 }
