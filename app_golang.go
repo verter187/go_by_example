f9e787a8a3596cb0692ba095d11c2250ada6c9cb
verter187 <verter187@inbox.ru>
2023-04-12 20:55:22 +0300

    Временные файлы и директории (Temporary Files and Directories)

--- app_golang.go
+++ app_golang.go
@@ -14,56 +14,23 @@ func check(e error) {
 
 func main() {
 
-	err := os.Mkdir("subdir", 0755)
+	f, err := os.CreateTemp("", "sample")
 	check(err)
 
-	defer os.RemoveAll("subdir")
+	fmt.Println("Temp file name:", f.Name())
 
-	createEmptyFile := func(name string) {
-		d := []byte("")
-		check(os.WriteFile(name, d, 0644))
-	}
+	defer os.Remove(f.Name())
 
-	createEmptyFile("subdir/file1")
-
-	err = os.MkdirAll("subdir/parent/child", 0755)
+	_, err = f.Write([]byte{1, 2, 3, 4})
 	check(err)
 
-	createEmptyFile("subdir/parent/file2")
-	createEmptyFile("subdir/parent/file3")
-	createEmptyFile("subdir/parent/child/file4")
-
-	c, err := os.ReadDir("subdir/parent")
+	dname, err := os.MkdirTemp("", "sampledir")
 	check(err)
+	fmt.Println("Temp dir name:", dname)
 
-	fmt.Println("Listing subdir/parent")
-	for _, entry := range c {
-		fmt.Println(" ", entry.Name(), entry.IsDir())
-	}
+	defer os.RemoveAll(dname)
 
-	err = os.Chdir("subdir/parent/child")
+	fname := filepath.Join(dname, "file1")
+	err = os.WriteFile(fname, []byte{1, 2}, 0666)
 	check(err)
-
-	c, err = os.ReadDir(".")
-	check(err)
-
-	fmt.Println("Listing subdir/parent/child")
-	for _, entry := range c {
-		fmt.Println(" ", entry.Name(), entry.IsDir())
-	}
-
-	err = os.Chdir("../../..")
-	check(err)
-
-	fmt.Println("Visiting subdir")
-	err = filepath.Walk("subdir", visit)
-	check(err)
-}
-
-func visit(p string, info os.FileInfo, err error) error {
-	if err != nil {
-		return err
-	}
-	fmt.Println(" ", p, info.IsDir())
-	return nil
 }
